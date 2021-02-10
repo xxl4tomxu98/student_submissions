63a285082be84639b8dced2aa6b3a60bfd7afe26
tom <tomxu@udel.edu>
2021-02-10 16:43:55 -0500

    database.py add seed data into database

--- service/database.py
+++ service/database.py
@@ -11,34 +11,54 @@ with app.app_context():
     db.create_all()
 
     ian = Student(school_id=1, grade_level='first_grade')
-
+    alissa = Student(school_id=2, grade_level='second_grade')
     db.session.add(ian)
-
-
-    s1 = Submission(course_work_id=1, course_id=1, student_id=1,
-                    type="TURNED_IN", assigned_points=90.0,
-                    max_points = 100.0)
-
-
-
-    db.session.add(s1)
-
-    # alissa.bookmarked_questions.append(q1)
-    # angela.bookmarked_questions.append(q2)
-    # db.session.add(alissa)
-    # db.session.add(angela)
+    db.session.add(alissa)
 
     c1 = Course(teacher_id=1, name = "algebra")
     c2 = Course(teacher_id=2, name = "geometry")
     c3 = Course(teacher_id=1, name = "literature")
 
-    # t2.tagged_questions.append(q2)
-    # t8.tagged_questions.append(q1)
     db.session.add(c1)
     db.session.add(c2)
     db.session.add(c3)
 
+    alissa.enrolled_courses.append(c1)
+    alissa.enrolled_courses.append(c3)
+    ian.enrolled_courses.append(c2)
+    db.session.add(alissa)
+    db.session.add(ian)
 
+    cw1 = Course_work(course_id=1, title="mid_term")
+    cw2 = Course_work(course_id=1, title="final_project")
+    cw3 = Course_work(course_id=2, title="quiz_1")
+    cw4 = Course_work(course_id=2, title="quiz_2")
+    cw5 = Course_work(course_id=3, title="mid_term")
+    cw6 = Course_work(course_id=3, title="quiz_1")
+    db.session.add(cw1)
+    db.session.add(cw2)
+    db.session.add(cw3)
+    db.session.add(cw4)
+    db.session.add(cw5)
+    db.session.add(cw6)
+
+    s1 = Submission(course_work_id=1, course_id=1, student_id=1,
+                    type="TURNED_IN", assigned_points=90.0,
+                    max_points = 100.0)
+    s2 = Submission(course_work_id=1, course_id=2, student_id=1,
+                    type="TURNED_IN", assigned_points=80.0,
+                    max_points = 100.0)
+    s3 = Submission(course_work_id=2, course_id=3, student_id=2,
+                    type="NEW", assigned_points=0.0,
+                    max_points = 100.0)
+    db.session.add(s1)
+    db.session.add(s2)
+    db.session.add(s3)
+
+
+
+
+    db.session.add(s1)
 
 
     db.session.commit()
